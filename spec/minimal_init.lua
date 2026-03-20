5a031054c583cebf41d07f31a8f22761afd154fa
Barrett Ruth <br.barrettruth@gmail.com>
2026-03-20 14:40:58 -0400

    fix: guard scp warning behind `vim.g.canola_ssh` and fix test init
    Problem: the scp:// deprecation warning fired for all users even if
    they never use SSH. The url_spec relied on global `config.init()` in
    `minimal_init.lua` which couples all tests to canola config state.
    
    Solution: only register the scp:// warning autocmd when
    `vim.g.canola_ssh` is configured. Move `config.init()` into the
    url_spec that actually needs it.

--- spec/minimal_init.lua
+++ spec/minimal_init.lua
@@ -1,5 +1,6 @@
 vim.cmd([[set runtimepath=$VIMRUNTIME]])
-local canola_path = vim.fn.expand('~/dev/canola.nvim')
+local canola_path = vim.uv.fs_stat('_canola') and '_canola'
+  or vim.fn.expand('~/dev/canola.nvim')
 vim.opt.runtimepath:append(canola_path)
 vim.opt.runtimepath:append('.')
 vim.opt.packpath = {}
@@ -8,4 +9,3 @@ vim.cmd('filetype on')
 vim.fn.mkdir(vim.fn.stdpath('cache'), 'p')
 vim.fn.mkdir(vim.fn.stdpath('data'), 'p')
 vim.fn.mkdir(vim.fn.stdpath('state'), 'p')
-require('canola.config').init()
