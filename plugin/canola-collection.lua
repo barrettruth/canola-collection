91ab209b49223d9485c81f5d2c0b8bebea8807c9
Barrett Ruth <br.barrettruth@gmail.com>
2026-04-04 11:00:30 -0400

    fix(trash): wire up delete.trash config to intercept file deletions
    Problem: the trash adapter's `delete_to_trash` function was never
    called. After extracting the trash adapter from canola core, nothing
    read the config flag or redirected file delete actions to trash.
    
    Solution: wrap the files adapter's `perform_action` and `render_action`
    in the plugin entry point. When `config.delete.trash` is true, delete
    actions on local files are redirected to the OS trash adapter, and the
    confirmation dialog shows "TRASH" instead of "DELETE".
    
    Requires barrettruth/canola.nvim@fix/delete-trash-config for the
    `delete.trash` config field.

--- plugin/canola-collection.lua
+++ plugin/canola-collection.lua
@@ -13,6 +13,35 @@ if vim.fn.has('nvim-0.12') == 0 then
   canola.register_adapter('canola-sss://', 's3')
 end
 
+local config = require('canola.config')
+local files = require('canola.adapters.files')
+local fs = require('canola.fs')
+local util = require('canola.util')
+
+local orig_perform = files.perform_action
+files.perform_action = function(action, cb)
+  if action.type == 'delete' and config.delete.trash then
+    local _, path = util.parse_url(action.url)
+    assert(path)
+    path = fs.posix_to_os_path(path)
+    require('canola.adapters.trash').delete_to_trash(path, cb)
+  else
+    orig_perform(action, cb)
+  end
+end
+
+local orig_render = files.render_action
+files.render_action = function(action)
+  if action.type == 'delete' and config.delete.trash then
+    local _, path = util.parse_url(action.url)
+    assert(path)
+    local short_path = files.to_short_os_path(path, action.entry_type)
+    return string.format(' TRASH %s', short_path)
+  else
+    return orig_render(action)
+  end
+end
+
 require('canola-git')._init()
 
 vim.api.nvim_create_autocmd('BufNew', {
